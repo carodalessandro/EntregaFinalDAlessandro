f265c47175bb5e9c813dc5173c5798eb348f0a76
Carolina <110741223+carodalessandro@users.noreply.github.com>
2023-05-21 00:30:55 -0300

    Update NavBar.jsx

--- Entrega/src/components/NavBar.jsx
+++ Entrega/src/components/NavBar.jsx
@@ -7,7 +7,7 @@ export function NavBar () {
 
     return(
       <nav className="navbar bg-black fontMenu nav" data-bs-theme="dark">
-        <img className="logo" src="/public/logo.png" alt="" />
+        <img className="logo" src="/logo.png" alt="" />
         <Link to='/'><button className='btn btn-black fontSize'>Productos</button></Link>
         <NavLink to='/category/cupcakes'><button className='fontSize btn btn-black'>Cupcakes</button></NavLink>
         <NavLink to='/category/tortas'><button className='btn btn-black fontSize'>Tortas</button></NavLink>
